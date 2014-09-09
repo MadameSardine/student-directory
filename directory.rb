0bce2770e44be90da5398460048f792c1e11a1e5
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-09 21:08:17 +0100

    Adding user input

--- directory.rb
+++ directory.rb
@@ -1,21 +1,21 @@
-#let's put all students into an array
-students = [
-{:name => "Bernard Mordan", :cohort => :september},
-{:name => "Stephens Hat", :cohort => :september},
-{:name => "Stephens Hat", :cohort => :september},
-{:name => "Nicole Pell", :cohort => :september},
-{:name => "Yvette Cook", :cohort => :september},
-{:name => "Elena Garrone", :cohort => :september},
-{:name => "Fadie", :cohort => :september},
-{:name => "Tim", :cohort => :september},
-{:name => "Ella Nancy Fay", :cohort => :september},
-{:name => "Andrew Hercules", :cohort => :september},
-{:name => "Camilla", :cohort => :september},
-{:name => "Pablo", :cohort => :september},
-{:name => "Rachel", :cohort => :september},
-{:name => "Anna Schechter", :cohort => :september},
-{:name => "Sandrine", :cohort => :september}
-]
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	#create an empty array
+	students = []
+	#gets the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {:name => name, :cohort => :september}
+		puts "Now we have #{students.length} students"
+		#get another name from the user
+		name = gets.chomp
+	end
+	#return the array of students
+	students
+end
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -33,6 +33,7 @@ puts "Overall it looks like we have #{names.length} students but some names may 
 end
 
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
