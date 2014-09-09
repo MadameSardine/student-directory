fa2599face312340808e8f988932d865d2769620
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-09 19:29:56 +0100

    Added comments to README.md

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts "The students of my cohort at Makers Academy"
 puts "------------"
 puts "Bernard Mordan"
@@ -14,6 +15,8 @@ puts "Pablo"
 puts "Rachel"
 puts "Anna Schechter"
 puts "Sandrine"
+#finally, we print the total
 print "Overall it looks like we have"
+#it's important that print() doesn't add new line characters
 print 14
 print " students but some names may be missing from the list."
