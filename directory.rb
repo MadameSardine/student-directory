e710c505317be9948e2bba74a6fecd8fa9595b50
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-10 10:26:54 +0100

    Only students names starting with 'a'

--- directory.rb
+++ directory.rb
@@ -6,11 +6,17 @@ def input_students
 	name = gets.chomp
 	# while the name is not empty, repeat this code
 	while !name.empty? do
+		# account only for names starting with an "a" or an "A"
+		if (name[0] == "a" || name[0] == "A") 
 		# add the student hash to the array
 		students << {:name => name, :cohort => :september}
 		print "Now we have #{students.length} students\n"
 		#get another name from the user
 		name = gets.chomp
+		else
+		#get another name from the user
+		name = gets.chomp
+	end
 	end
 	#return the array of students
 	students
