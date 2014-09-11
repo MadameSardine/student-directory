74b814f081dc394e70c34f7fa7a9dc9fb23fa231
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-11 18:27:56 +0100

    Saving data to a file

--- directory.rb
+++ directory.rb
@@ -2,39 +2,34 @@
 @students_cohort = []
 @months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-def print_menu
-	puts "1. Input the students"
-	puts "2. Show the students"
-	puts "9. Exit"
+def print_header
+	print "The students of my cohort at Makers Academy\n-------------------\n"
 end
 
-def show_students
-	print_header
-	print_students_list
-	print_footer
+def print_students_list
+	@students.each do |student|
+		puts "\t*#{student[:name].capitalize}, (#{student[:cohort]} cohort)"
+	end
 end
 
-def process(selection)
-	case selection
-		when "1"
-			# input the students
-			input_students
-		when "2"
-			# show the students
-			show_students
-		when "9"
-			exit # this will cause the program to terminate
-		else
-			puts "I don't know what you meant, try again"
-	end	
+def print_by_cohort
+	@months.each do |month|
+		puts "Students in the #{month} cohort:"
+		@students_cohort = @students.select {|student| month == student[:cohort]}
+		@students_cohort.each do |students_cohort|
+			puts "\t*#{students_cohort[:name].capitalize}"
+		end
+	end
 end
 
-def interactive_menu
-	loop do
-		# 1. print the menu and ask the user what to do
-		print_menu
-		# 2. read the input and do what the user has asked
-		process(gets.chomp)
+def print_footer
+	puts "Overall it looks like we have #{@students.length} #{students_form} but some names may be missing from the list."
+end
+
+def students_form 
+	if @students.length == 1
+		then "student"
+	else "students"
 	end
 end
 
@@ -70,36 +65,55 @@ def input_students
 	@students
 end
 
-def print_header
-	print "The students of my cohort at Makers Academy\n-------------------\n"
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
+	puts "9. Exit"
 end
 
-def print_students_list
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def process(selection)
+	case selection
+		when "1"
+			# input the students
+			input_students
+		when "2"
+			# show the students
+			show_students
+		when "3"
+			save_students
+		when "9"
+			exit # this will cause the program to terminate
+		else
+			puts "I don't know what you meant, try again"
+	end	
+end
+
+def interactive_menu
+	loop do
+		# 1. print the menu and ask the user what to do
+		print_menu
+		# 2. read the input and do what the user has asked
+		process(gets.chomp)
+	end
+end
+
+def save_students
+	# open the file for writing
+	file = File.open("students.csv", "w")
+	# iterate over the array of students
 	@students.each do |student|
-		puts "\t*#{student[:name].capitalize}, (#{student[:cohort]} cohort)"
+		student_data = [student[:name], student[:cohort]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
 	end
+	file.close
 end
-
-def print_by_cohort
-	@months.each do |month|
-		puts "Students in the #{month} cohort:"
-		@students_cohort = @students.select {|student| month == student[:cohort]}
-		@students_cohort.each do |students_cohort|
-			puts "\t*#{students_cohort[:name].capitalize}"
-		end
-	end
-end
-
-def print_footer
-	puts "Overall it looks like we have #{@students.length} #{students_form} but some names may be missing from the list."
-end
-
-def students_form 
-	if @students.length == 1
-		then "student"
-	else "students"
-	end
-end
-
 
 interactive_menu
